ac46f360dd1611c584b29c1be9834920db356c9d
piq9117 <piq9117@noreply.example.org>
2020-05-20 04:57:23 +0000

    refactor: app representation with readert (#45)
    - exposed the dependency in app representation by using ReaderT

--- src/UmuHalogen.hs
+++ src/UmuHalogen.hs
@@ -1,6 +1,7 @@
+{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
-module UmuHalogen where
+module UmuHalogen ( startApp ) where
 
 import           Control.Monad.Except
 import           Import
@@ -13,30 +14,31 @@ import           UmuHalogen.Parser.Command
 
 newtype AppM m a
   = AppM
-  { unAppM :: ( ExceptT UmuError m ) a
-  } deriving ( Functor, Applicative, Monad, MonadIO, MonadError UmuError )
+  { unAppM :: ReaderT Command ( ExceptT UmuError m ) a
+  } deriving ( Functor, Applicative, Monad, MonadIO, MonadError UmuError, MonadReader Command )
 
-runAppM :: MonadIO m => AppM m a -> ExceptT UmuError m a
-runAppM app = unAppM app
+runAppM :: MonadIO m => Command -> ExceptT UmuError m [ UmuResponse ]
+runAppM comm = runReaderT ( unAppM $ convertApp comm ) comm
+
+convertApp :: MonadIO m => Command -> AppM m [ UmuResponse ]
+convertApp comm =
+  case comm of
+    CommandInit mPath -> generateProject mPath
+    CommandComponent path componentName -> either
+      throwError
+      ( ( pure <$> ) . flip generateComponent componentName )
+      path
 
 startApp :: IO ()
 startApp = do
   comm <- showHelpOnErrorExecParser
     ( info ( helper <*> parseVersion <*> parseCommand )
       ( fullDesc <> progDesc umuProgDesc <> header umuHeader ))
-  res <- runExceptT $ runAppM $ runCommand comm
+  res <- runExceptT $ runAppM comm
   either
     ( logError . umuErrorToText )
     ( traverse_ ( logInfo . umuResponseToText ) )
     res
-  where
-    runCommand :: MonadIO m => Command -> AppM m [ UmuResponse ]
-    runCommand comm = case comm of
-      CommandInit mPath -> generateProject mPath
-      CommandComponent path componentName -> either
-        throwError
-        ( ( pure <$> ) . flip generateComponent componentName )
-        path
 
 instance MonadIO m => ManageGeneration ( AppM m ) where
   generateProject = genProject
